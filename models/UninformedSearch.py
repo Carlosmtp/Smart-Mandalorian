81c3e49b786780226f77291884018aa577b821d4
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2024-04-09 14:09:28 -0500

    Corrección al contar los costos y nodos en UninformedSearch.py

--- models/UninformedSearch.py
+++ models/UninformedSearch.py
@@ -20,19 +20,21 @@ def BreadthFirstSearch(world):
     tree = []
     tree.append(root)
     explored = set()
+    tree_nodes = 1
     expanded_nodes = 0
     start_time = time.time()
     while tree:
         current = tree.pop(0)
+        expanded_nodes += 1
         if is_grogu(current, world):
             end_time = time.time()
             path, cost = reconstruct_solution(current)
             computation_time = end_time - start_time
-            return path, expanded_nodes, len(path)-1, computation_time, cost
+            return path, tree_nodes, expanded_nodes, len(path), computation_time, cost
         else:
             for next in current.possible_moves():
                 if next not in explored:
-                    expanded_nodes += 1
+                    tree_nodes += 1
                     tree.append(next)
                     explored.add(next)
 
@@ -54,20 +56,22 @@ def UniformCostSearch(world):
     tree = []
     tree.append(root)
     explored = set()
+    tree_nodes = 1
     expanded_nodes = 0
     start_time = time.time()
     while tree:
         tree.sort(key=lambda x: x.cost)
         current = tree.pop(0)
+        expanded_nodes += 1
         if is_grogu(current, world):
             end_time = time.time()
             path, cost = reconstruct_solution(current)
             computation_time = end_time - start_time
-            return path, expanded_nodes, len(path)-1, computation_time, cost
+            return path, tree_nodes, expanded_nodes, len(path), computation_time, cost
         else:
             for next in current.possible_moves():
                 if next not in explored:
-                    expanded_nodes += 1
+                    tree_nodes += 1
                     tree.append(next)
                     explored.add(next)
 
@@ -90,20 +94,22 @@ def DepthFirstSearch(world):
     tree.append(root)
     explored = set()
     explored_in_branch = set()
+    tree_nodes = 1
     expanded_nodes = 0
     start_time = time.time()
     while tree:
         current = tree.pop()
+        expanded_nodes += 1
         explored_in_branch = set()
         if is_grogu(current, world):
             end_time = time.time()
             path, cost = reconstruct_solution(current)
             computation_time = end_time - start_time
-            return path, expanded_nodes, len(path)-1, computation_time, cost
+            return path, tree_nodes, expanded_nodes, len(path), computation_time, cost
         else:
             for next in current.possible_moves():
                 if next not in explored and next not in explored_in_branch:
-                    expanded_nodes += 1
+                    tree_nodes += 1
                     tree.append(next)
                     explored_in_branch.add(next)
             explored.add(current)
